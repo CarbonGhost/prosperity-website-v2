ad4d9cf10dcc094e00e7934169459eaf5a17dc40
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2022-11-04 15:30:43 +0800

    new: Routing UI
    Note that this implementation doesn't work very well, I believe, due to state not being propagated, will fix.

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from "@solidjs/router"
 import { lazy } from "solid-js"
 
+import WikiPage from "./routes/wiki/[page]"
+
 export default function AppRoutes() {
 	return (
 		<Routes>
@@ -9,10 +11,7 @@ export default function AppRoutes() {
 				path="/projects"
 				component={lazy(() => import("./routes/projects"))}
 			/>
-			<Route
-				path="/wiki/:page"
-				component={lazy(() => import("./routes/wiki/[page]"))}
-			/>
+			<Route path="/wiki/:page" component={WikiPage} />
 			<Route
 				path={["/wiki", "/wiki/home"]}
 				element={<Navigate href="/wiki/index" />}
--- src/routes/wiki/[page].tsx
+++ src/routes/wiki/[page].tsx
@@ -2,6 +2,8 @@ import { useParams } from "@solidjs/router"
 import { JSXElement, lazy } from "solid-js"
 import { Suspense } from "solid-js"
 
+import DynamicLink from "../../components/DynamicLink"
+
 export default function Wiki(): JSXElement {
 	const route = useParams()
 	// The wiki page content. Will navigate to the index page should the route be
@@ -9,13 +11,36 @@ export default function Wiki(): JSXElement {
 	const Content = lazy(() => import(`../../content/${route.page}.mdx`))
 
 	return (
-		<div class="standard-width">
-			<article>
+		<div class="standard-width md:flex space-y-6 md:space-y-0 md:gap-6">
+			<Sidebar />
+			<article class="md:ml-48 w-4/5">
 				{/* Use `Suspense` to wrap the dynamic import from `Content`. */}
 				<Suspense>
 					<Content />
 				</Suspense>
 			</article>
+		</div>
+	)
+}
+
+function Sidebar(): JSXElement {
+	const links: { displayName: string; href: string }[] = [
+		{ displayName: "Index", href: "/wiki/index" },
+		{ displayName: "Valid", href: "/wiki/valid" },
+	]
+
+	return (
+		<div class="bg-zinc-700 md:max-w-40 h-min md:fixed border-zinc-500 w-full md:w-40 px-6 border rounded-md shadow-lg">
+			<div class="grid">
+				{links.map((i) => (
+					<DynamicLink
+						class="text-zinc-200 decoration-zinc-500 py-1.5 self-center transform hover:text-zinc-300 duration-150"
+						href={i.href}
+						activeClass="underline">
+						{i.displayName}
+					</DynamicLink>
+				))}
+			</div>
 		</div>
 	)
 }
