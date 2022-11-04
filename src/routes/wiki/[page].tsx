d2750b47fd159ba98335b868e6a6ed916a4276b5
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2022-11-04 19:49:01 +0800

    new: Add categories to the wiki sidebar

--- src/routes/wiki/[page].tsx
+++ src/routes/wiki/[page].tsx
@@ -11,9 +11,9 @@ export default function Wiki(): JSXElement {
 	const Content = lazy(() => import(`../../content/${route.page}.mdx`))
 
 	return (
-		<div class="standard-width md:flex space-y-6 md:space-y-0 md:gap-6">
+		<div class="standard-width md:flex md:space-y-0 md:gap-6 space-y-6">
 			<Sidebar />
-			<article class="md:ml-48 w-4/5">
+			<article class="md:ml-60">
 				{/* Use `Suspense` to wrap the dynamic import from `Content`. */}
 				<Suspense>
 					<Content />
@@ -24,23 +24,63 @@ export default function Wiki(): JSXElement {
 }
 
 function Sidebar(): JSXElement {
-	const links: { displayName: string; href: string }[] = [
-		{ displayName: "Index", href: "/wiki/index" },
-		{ displayName: "Valid", href: "/wiki/valid" },
+	const links: {
+		category: string
+		subLinks: { displayName: string; href: string }[]
+	}[] = [
+		{
+			category: "",
+			subLinks: [{ displayName: "Index", href: "/wiki/index" }]
+		},
+		{
+			category: "Rules & Administration",
+			subLinks: [
+				{
+					displayName: "Admin Overview",
+					href: "/wiki/administration-overrview"
+				},
+				{
+					displayName: "Allowed Mods",
+					href: "/wiki/allowed-mods"
+				},
+				{
+					displayName: "Server Rules",
+					href: "/wiki/rules"
+				}
+			]
+		},
+		{
+			category: "Guides",
+			subLinks: [
+				{
+					displayName: "New Player Guide",
+					href: "/wiki/new-player-guide"
+				},
+				{
+					displayName: "OptiFine Alternatives",
+					href: "/wiki/optifine-alternatives"
+				}
+			]
+		}
 	]
 
 	return (
-		<div class="bg-zinc-700 md:max-w-40 h-min md:fixed border-zinc-500 w-full md:w-40 px-6 border rounded-md shadow-lg">
-			<div class="grid">
-				{links.map((i) => (
-					<DynamicLink
-						class="text-zinc-200 decoration-zinc-500 py-1.5 self-center transform hover:text-zinc-300 duration-150"
-						href={i.href}
-						activeClass="underline">
-						{i.displayName}
-					</DynamicLink>
-				))}
-			</div>
+		<div class="bg-zinc-700 h-min md:fixed border-zinc-500 w-in px-6 border rounded-md shadow-lg">
+			{links.map((i) => (
+				<>
+					<header class="text-xs">{i.category}</header>
+					<section class="grid">
+						{i.subLinks.sort().map((i2) => (
+							<DynamicLink
+								class="text-zinc-200 decoration-zinc-500 py-1.5 self-center transform hover:text-zinc-300 duration-150"
+								href={i2.href}
+								activeClass="underline">
+								{i2.displayName}
+							</DynamicLink>
+						))}
+					</section>
+				</>
+			))}
 		</div>
 	)
 }
