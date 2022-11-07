a77df19d1995bbaa0e4195a206f0180b371d74c9
CarbonGhost <39361743+CarbonGhost@users.noreply.github.com>
2022-11-07 18:50:03 +0800

    fix: Lazy load `/` route

--- src/AppRoutes.tsx
+++ src/AppRoutes.tsx
@@ -2,12 +2,11 @@ import { Routes, Route, Navigate } from "@solidjs/router"
 import { lazy } from "solid-js"
 
 import WikiData from "./routes/wiki/Wiki.data"
-import Home from "./routes/Home"
 
 export default function AppRoutes() {
 	return (
 		<Routes>
-			<Route path="/" component={Home} />
+			<Route path="/" component={lazy(() => import("./routes/Home"))} />
 			<Route
 				path="/projects"
 				component={lazy(() => import("./routes/projects"))}
